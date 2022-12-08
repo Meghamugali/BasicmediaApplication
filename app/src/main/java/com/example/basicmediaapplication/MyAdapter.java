88eb9d2334782aacc1ca7fc931933d5cd97be765
Megha <meghabmugali@gmail.com>
2022-12-08 13:30:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basicmediaapplication/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.example.basicmediaapplication;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String data1[];
+    int images[];
+    Context context;
+
+    public MyAdapter(Context ct, String[] s1, int[] img){
+       context = ct;
+       data1 = s1;
+       images = img;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v= LayoutInflater.from(context).inflate(R.layout.my_row,parent,false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.textView1.setText(data1[position]);
+        holder.imageView.setImageResource(images[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textView1;
+        ImageView imageView;
+
+        public  MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            textView1 = itemView.findViewById(R.id.textView1);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+
+}
