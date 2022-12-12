e133086e35463a4ea88655269e5ffb2a16d9ef39
Megha <meghabmugali@gmail.com>
2022-12-12 13:02:58 +0530

    Initial commit

--- app/src/main/java/com/example/basicmediaapplication/Adapter.java
+++ app/src/main/java/com/example/basicmediaapplication/Adapter.java
@@ -1,11 +1,13 @@
 package com.example.basicmediaapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -43,7 +45,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
         return images.length;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         TextView textView1 , textView2;
         ImageView imageView;
@@ -53,6 +57,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
             textView1 = itemView.findViewById(R.id.textView1);
             textView2 = itemView.findViewById(R.id.textView2);
             imageView = itemView.findViewById(R.id.imageView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+//            int position = getAdapterPosition();
+//            Toast.makeText(view.getContext(), "position"+position, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(view.getContext(),PrintandgiftActivity.class);
+            view.getContext().startActivity(intent);
+
         }
     }
 }
---
+++ app/src/main/java/com/example/basicmediaapplication/LibraryActivity.java
@@ -0,0 +1,125 @@
+package com.example.basicmediaapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.basicmediaapplication.databinding.ActivityMainBinding;
+
+public class LibraryActivity extends AppCompatActivity {
+    TextView uri_tv;
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        uri_tv = findViewById(R.id.uri_tv);
+        setContentView(R.layout.activity_library);
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+    public void pickVideosFromStorage(View view) {
+        if (isPermissionGranted()){
+            pickAllVideosFromStorage();
+        } else {
+            getPermission();
+        }
+    }
+
+    private boolean isPermissionGranted(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            return Environment.isExternalStorageManager();
+        }else {
+            int readExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            return  readExternalStoragePermission == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    private void getPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse(String.format("package%s", getApplicationContext())));
+                startActivityForResult(intent, 101);
+            } catch (Exception exception) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                startActivityForResult(intent, 101);
+            }
+        }else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if(requestCode == 101) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    if(Environment.isExternalStorageManager()){
+                        pickAllVideosFromStorage();
+                    } else {
+                        getPermission();
+                    }
+                }
+            } else if (requestCode == 103){
+                Uri uri = data.getData();
+                if (uri != null){
+                    uri_tv.setText(uri.getPath());
+                } else {
+                    return;
+                }
+            }
+        }
+    }
+
+    private void pickAllVideosFromStorage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(intent,103);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100){
+            if (grantResults.length > 0) {
+                boolean isReadExternalStoragePermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                if (isReadExternalStoragePermission){
+                    pickAllVideosFromStorage();
+                } else {
+                    getPermission();
+                }
+            }
+
+        }
+    }
+}
